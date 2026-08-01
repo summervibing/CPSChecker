ead0e7cd67b47af6720abd2b5b19f37461fb8813
summervibing <57875937+summervibing@users.noreply.github.com>
2026-08-01 16:04:45 +0200

    build(gradle): configure Java 17 toolchain for all subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,12 @@ subprojects {
         compileOnly(libs.protocollib)
     }
 
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(17)
+        }
+    }
+
     tasks.compileJava {
         options.encoding = Charsets.UTF_8.name()
     }
