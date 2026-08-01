beb0b707e09032c8175d38ff793a2edb5bc3056e
Marvin <57875937+summervibing@users.noreply.github.com>
2026-08-01 03:34:19 +0200

    chore: remove test server files

--- test-server/start.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Java\jdk-17\bin\java.exe" -version
-"C:\Program Files\Java\jdk-17\bin\java.exe" -Xmx2G -jar spigot-1.8.8.jar
-PAUSE
--- test-server/start.sh
+++
@@ -1,1 +0,0 @@
-"/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home/bin/java" -Xmx2G -jar spigot-1.8.8.jar
